f1f5968d85382d9e0d636b9956d1eb09177df959
jasthi-navya <106503279+jasthi-navya@users.noreply.github.com>
2023-06-14 22:19:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+// JavaScript
+
+// Function to redirect to the blogs page
+function redirectToBlogs() {
+    window.location.href = "blogs.html";
+  }
+
+  function redirectToHome() {
+    window.location.href = "index.html";
+  }
+  
+  // Function to display a "Not yet updated" message
+  function showNotUpdatedMessage() {
+    window.location.href = "others.html";
+  }
+
+  var homeLink = document.getElementById("home-link");
+  homeLink.addEventListener("click", redirectToHome);
+  
+  // Event listeners for the navigation links
+  var blogsLink = document.getElementById("blogs-link");
+  blogsLink.addEventListener("click", redirectToBlogs);
+  
+  var newsLink = document.getElementById("news-link");
+  newsLink.addEventListener("click", showNotUpdatedMessage);
+  
+  var eventsLink = document.getElementById("events-link");
+  eventsLink.addEventListener("click", showNotUpdatedMessage);
+  
+  var jobsLink = document.getElementById("jobs-link");
+  jobsLink.addEventListener("click", showNotUpdatedMessage);
+
+  var a = document.getElementById("a-link");
+  a.addEventListener("click", showNotUpdatedMessage);
+
+  var b = document.getElementById("b-link");
+  b.addEventListener("click", showNotUpdatedMessage);
+
+  var c = document.getElementById("c-link");
+  c.addEventListener("click", showNotUpdatedMessage);
+
+  var d = document.getElementById("d-link");
+  d.addEventListener("click", showNotUpdatedMessage);
+
+  var e = document.getElementById("e-link");
+  e.addEventListener("click", showNotUpdatedMessage);
+
+  var f = document.getElementById("f-link");
+  f.addEventListener("click", showNotUpdatedMessage);
+
+  var g = document.getElementById("g-link");
+  g.addEventListener("click", showNotUpdatedMessage);
+
+  var h = document.getElementById("h-link");
+  h.addEventListener("click", showNotUpdatedMessage);
+
+  var i = document.getElementById("i-link");
+  i.addEventListener("click", showNotUpdatedMessage);
+
+  var j = document.getElementById("j-link");
+  j.addEventListener("click", showNotUpdatedMessage);
+
+  var k = document.getElementById("k-link");
+  k.addEventListener("click", showNotUpdatedMessage);
+
+  var l = document.getElementById("l-link");
+  l.addEventListener("click", showNotUpdatedMessage);
+
+  var m = document.getElementById("jm-link");
+  m.addEventListener("click", showNotUpdatedMessage);
+
+  var n = document.getElementById("n-link");
+  n.addEventListener("click", showNotUpdatedMessage);
+
+  var o = document.getElementById("o-link");
+  o.addEventListener("click", showNotUpdatedMessage);s
+  
